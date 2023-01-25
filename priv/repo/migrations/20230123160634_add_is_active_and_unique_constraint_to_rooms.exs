a1d187f6e65e9d5d1dea3ee06d4322751e7e7a88
E.Azer Koçulu <azer@kodfabrik.com>
2023-01-25 12:43:57 +0100

    fix typo

--- priv/repo/migrations/20230123160634_add_is_active_and_unique_constraint_to_rooms.exs
+++ priv/repo/migrations/20230123160634_add_is_active_and_unique_constraint_to_rooms.exs
@@ -2,7 +2,7 @@ defmodule Bafa.Repo.Migrations.AddIsActiveAndUniqueConstraintToRooms do
   use Ecto.Migration
 
   def change do
-    walter table(:rooms) do
+    alter table(:rooms) do
       add :is_active, :boolean, default: true
     end
     create unique_index(:rooms, [:slug, :org_id])
