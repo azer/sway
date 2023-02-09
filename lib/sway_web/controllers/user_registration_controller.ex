e7689b4a2de6cf0dffe6de437d51c2a639e26281
E.Azer Koçulu <azer@kodfabrik.com>
2023-02-09 12:55:25 +0100

    HTML email

--- lib/sway_web/controllers/user_registration_controller.ex
+++ lib/sway_web/controllers/user_registration_controller.ex
@@ -23,11 +23,15 @@ defmodule SwayWeb.UserRegistrationController do
     changeset = Accounts.change_user_registration(%User{})
     oauth_google_url = ElixirAuthGoogle.generate_oauth_url(conn)
 
-    conn = if invite do
-      conn
-      |> put_flash(:info, "You're invited by #{invite.created_by.name} to join #{invite.workspace.name}.")
-    else
-      conn
+    conn = cond do
+      invite ->
+	conn
+	|> put_flash(:info, "You're invited by #{invite.created_by.name} to join #{invite.workspace.name}.")
+      invite_invalid ->
+	conn
+	|> put_flash(:error, "Oops, your invite seems to be invalid.")
+      true ->
+	conn
     end
 
     render(conn, "new.html", changeset: changeset, invite: invite, invite_invalid: invite_invalid, oauth_google_url: oauth_google_url)
--- lib/sway_web/mail/invites.ex
+++ lib/sway_web/mail/invites.ex
@@ -7,6 +7,24 @@ defmodule SwayWeb.InviteEmail do
     |> to({invite.name, invite.email})
     |> from(Sway.Mailer.from())
     |> subject("#{invite.created_by.name} invited you to #{invite.workspace.name}")
-    |> text_body("Hey #{invite.name},\n\n#{invite.created_by.name} invited you to #{invite.workspace.name}. Click the link below: https://sway.so/join?invite=#{token} \n\nSway - Video chat rooms for remote teams.")
+    |> html_body(~s"""
+    <div style="background-color:#fff; padding: 20px 30px; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;">
+      <div>Hey #{invite.name},</div>
+      <div style="margin: 10px 0;">
+        <strong>#{invite.created_by.name}</strong> invited you to
+        <strong>#{invite.workspace.name}</strong>.
+      </div>
+      <a href="https://sway.so/join?invite=#{token}" style="color: rgb(38, 150, 255); display: inline-block;"
+        >Click here to join.</a
+      >
+      <div style="margin-top: 30px;">
+        <img src="https://cldup.com/HIL47P0PRk.png" style="width: 35px;" />
+        <div style="font-size: 13px; color: #888; line-height: 18px">
+          <a href="https://sway.so" style="color: #888;">Sway.so</a>, remote communication<br /> with
+          in-person feel.
+        </div>
+      </div>
+    </div>
+    """)
   end
 end
