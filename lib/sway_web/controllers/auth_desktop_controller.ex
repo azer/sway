dd3245f842f913eccc373d66aefae380ac925310
E.Azer Koçulu <azer@kodfabrik.com>
2023-05-15 11:01:58 +0200

    Handle auth errors

--- lib/sway_web/controllers/auth_desktop_controller.ex
+++ lib/sway_web/controllers/auth_desktop_controller.ex
@@ -5,8 +5,12 @@ defmodule SwayWeb.AuthDesktopController do
   @default_scope "email profile"
   @request_path "/desktop/auth/oauth_login"
   @callback_path "/desktop/auth/oauth_callback"
-  @provider_config {Ueberauth.Strategy.Google, [default_scope: @default_scope, request_path: @request_path, callback_path: @callback_path]}
-
+  @provider_config {Ueberauth.Strategy.Google,
+                    [
+                      default_scope: @default_scope,
+                      request_path: @request_path,
+                      callback_path: @callback_path
+                    ]}
 
   def desktop_redirect(conn, _params) do
     {:ok, key, claims} = Guardian.encode_and_sign(conn.assigns.current_user, %{}, auth_time: true)
@@ -18,43 +22,50 @@ defmodule SwayWeb.AuthDesktopController do
     |> Ueberauth.run_request("google", @provider_config)
   end
 
-  def auth_start(conn, %{ "key" => key } = params) do
+  def auth_start(conn, %{"key" => key} = params) do
     case Guardian.decode_and_verify(key, %{}, max_age: {1, :hours}) do
       {:ok, claims} ->
-	{:ok, user, claims} = Guardian.resource_from_token(key)
+        {:ok, user, claims} = Guardian.resource_from_token(key)
 
-	case SwayWeb.UserOauthController.login_by_email(user.email, user) do
-	  {:ok, user, workspace} ->
-	     conn
-             |> put_session(:user_return_to, "/#{workspace.slug}")
-             |> SwayWeb.UserAuth.log_in_user(user)
+        case SwayWeb.UserOauthController.login_by_email(user.email, user) do
+          {:ok, user, workspace} ->
+            conn
+            |> put_session(:user_return_to, "/#{workspace.slug}")
+            |> SwayWeb.UserAuth.log_in_user(user)
 
-	  {:error, _reason} ->
-	     conn
-             |> put_flash(:error, "Oops! You don't have an invite to join Sway yet.")
-             |> redirect(to: "/login")
-	end
+          {:error, _reason} ->
+            conn
+            |> put_flash(:error, "Oops! You don't have an invite to join Sway yet.")
+            |> redirect(to: "/login")
+        end
 
       {:error, :token_expired} ->
-	 conn
+        conn
         |> put_flash(:error, "Oops! There token is expired.")
         |> redirect(to: "/login")
     end
   end
 
   def oauth_callback(conn, params) do
-    conn = Ueberauth.run_callback(conn, "google", @provider_config)
-    %{assigns: %{ueberauth_auth: %{info: user_info}}} = conn
+    case Ueberauth.run_callback(conn, "google", @provider_config) do
+      %{assigns: %{ueberauth_auth: %{info: user_info}}} ->
+        case SwayWeb.UserOauthController.login_by_email(user_info.email, user_info) do
+          {:ok, user, workspace} ->
+            conn
+            |> put_session(:user_return_to, "/desktop/auth/redirect")
+            |> SwayWeb.UserAuth.log_in_user(user)
 
-    case SwayWeb.UserOauthController.login_by_email(user_info.email, user_info) do
-      {:ok, user, workspace} ->
-        conn
-        |> put_session(:user_return_to, "/desktop/auth/redirect")
-        |> SwayWeb.UserAuth.log_in_user(user)
+          {:error, _reason} ->
+            conn
+            |> put_flash(:error, "Oops! You don't have an invite to join Sway yet.")
+            |> redirect(to: "/login")
+        end
 
-      {:error, _reason} ->
+      %{assigns: %{ueberauth_failure: failure}} ->
+        error_message = failure.error_message || failure.errors |> List.first() |> Map.get(:message) || "An unknown error occurred."
+
         conn
-        |> put_flash(:error, "Oops! You don't have an invite to join Sway yet.")
+        |> put_flash(:error, "Failed to authenticate. Reason: #{error_message}")
         |> redirect(to: "/login")
     end
   end
